34adadc87a0c565afaef36b2fb06ebcb57c389cd
Adamshafi007 <sdmdwd1710@gmail.com>
2021-04-20 07:02:18 +0530

    email

--- src/main/java/Pages/UserDetailsPage.java
+++ src/main/java/Pages/UserDetailsPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class UserDetailsPage extends BaseFlight {
-    @FindBy(name="inputName")
+    @FindBy(id="inputName")
     WebElement firstName;
 
     @FindBy(id="address")
